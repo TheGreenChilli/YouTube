a2acdae246d98ec5a8577cffbb93164c126e34e6
sanjana <sanju.kri2112@gmail.com>
2025-04-25 11:33:46 +0530

    Add Tailwind CSS integration and restructure components
    - Added Tailwind CSS as a dependency in package.json and package-lock.json.
    - Created postcss.config.js for PostCSS configuration.
    - Restructured App component to use Head and Body components.
    - Implemented Body component with Sidebar and MainContainer.
    - Added Sidebar, MainContainer, ButtonList, VideoContainer, and Head components.
    - Updated App.css to utilize Tailwind CSS styles.
    - Created tailwind.config.js for Tailwind CSS configuration.

---
+++ src/components/Sidebar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="p-5 shadow-lg w-48">
+      <h1>Subscriptions</h1>
+      <ul>
+        <h1>Music</h1>
+        <h1>Sports</h1>
+        <h1>Gaming</h1>
+        <h1>Movies</h1>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
